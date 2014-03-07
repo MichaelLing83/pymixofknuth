10401c6710e10c92c72dd9b663721b7f39f15efb
duo.ling.cn@gmail.com <duo.ling.cn@gmail.com>
2014-03-07 16:20:04 +0100

    Add registers in MMIX.

--- src/MMIX.py
+++ src/MMIX.py
@@ -1,8 +1,13 @@
-﻿
-from bitstring import BitArray
+﻿from Register import Register
 
 class MMIX:
     ADDRESS_WIDTH_IN_BIT = 64
     REGISTER_BIT_WIDTH = 64
     def __init__(self):
-        
+        self.general_purpose_registers = list()
+        for i in range(256):
+            self.general_purpose_registers.append(Register())
+        self.special_purpose_registers = list()
+        for i in range(32):
+            self.special_purpose_registers.append(Register())
+        self.special_purpose_register_names = ['rB', 'rD', 'rE', 'rH', 'rJ', 'rM', 'rR', 'rBB', 'rC', 'rN', 'rO', 'rS', 'rI', 'rT', 'rTT', 'rK', 'rQ', 'rU', 'rV', 'rG', 'rL', 'rA', 'rF', 'rP', 'rW', 'rX', 'rY', 'rZ', 'rWW', 'rXX', 'rYY', 'rZZ']
---
+++ src/testMMIX.py
@@ -0,0 +1,30 @@
+﻿import unittest
+from MMIX import MMIX
+
+class TestOcta(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("\nStart testing %s" % __name__)
+    
+    @classmethod
+    def tearDownClass(cls):
+        print("\nFinish testing %s" % __name__)
+    
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    def testInit(self):
+        '''
+        Verify that Octa instances can be correctly initialized.
+        '''
+        mmix = MMIX()
+        self.assertEqual(len(mmix.general_purpose_registers), 256)
+        self.assertEqual(len(mmix.special_purpose_registers), 32)
+        self.assertEqual(len(mmix.special_purpose_register_names), 32)
+
+if __name__ == '__main__':
+    unittest.main()
