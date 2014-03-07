5a2d4d29381b150328b53fd746a6117cd2f1e0bc
Duo Ling <duo.ling@tieto.com>
2014-03-07 13:58:33 +0100

    Rename class Word to Wyde -- storage for 2 Bytes.

---
+++ src/Wyde.py
@@ -0,0 +1,30 @@
+﻿from bitstring import BitArray
+
+class Wyde:
+    WYDE_SIZE_IN_BIT = 16   # bits
+    def __init__(self, int=0, uint=0):
+        self.Wyde = BitArray(length=Wyde.WYDE_SIZE_IN_BIT, uint=0)
+        self.length = self.Wyde.length
+        if int!=0 and uint==0:
+            self.Wyde.int = int
+        elif int==0 and uint!=0:
+            self.Wyde.uint = uint
+        self.int = self.Wyde.int
+        self.uint = self.Wyde.uint
+        self.bin = self.Wyde.bin
+        self.hex = self.Wyde.hex
+    
+    def __and__(self, another_Wyde):
+        return Wyde(uint=(self.Wyde & another_Wyde.Wyde).uint)
+    
+    def __or__(self, another_Wyde):
+        return Wyde(uint=(self.Wyde | another_Wyde.Wyde).uint)
+    
+    def __xor__(self, another_Wyde):
+        return Wyde(uint=(self.Wyde ^ another_Wyde.Wyde).uint)
+    
+    def __add__(self, another_Wyde):
+        return Wyde(int=(self.Wyde.int + another_Wyde.Wyde.int))
+    
+    def __sub__(self, another_Wyde):
+        return Wyde(int=(self.Wyde.int - another_Wyde.Wyde.int))
---
+++ src/testWyde.py
@@ -0,0 +1,83 @@
+﻿import unittest
+from bitstring import BitArray
+from bitstring import CreationError
+from Wyde import Wyde
+from random import randint
+
+WYDE_SIZE = 16  # bits
+
+class TestWyde(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    def testInit(self):
+        '''
+        Verify that Wyde instances can be correctly initialized.
+        '''
+        wyde = Wyde()
+        self.assertEqual(wyde.length, WYDE_SIZE, "Size of one Wyde must be %d bits, it is %d bits instead." % (WYDE_SIZE, wyde.length))
+        self.assertEqual(wyde.int, 0, "By default, Wyde should be initialized to zero, it is %d in decimal instead." % wyde.int)
+        self.assertEqual(wyde.bin, '0'*WYDE_SIZE, "By default, Wyde should be initialized to zero, it is %s in binary instead." % wyde.bin)
+        self.assertEqual(wyde.hex, '0'*int(WYDE_SIZE/4))
+        
+        # Initialize with valid signed values
+        test_values = list()
+        test_values.append(-2**(WYDE_SIZE-1))
+        test_values.append(2**(WYDE_SIZE-1)-1)
+        for i in range(20):
+            test_values.append(randint(-2**(WYDE_SIZE-1)+1, 2**(WYDE_SIZE-1)-1-1))
+        for i in test_values:
+            reference = BitArray(int=i, length=WYDE_SIZE)
+            wyde = Wyde(int=i)
+            self.assertEqual(wyde.length, WYDE_SIZE)
+            self.assertEqual(wyde.int, i)
+            self.assertEqual(wyde.bin, reference.bin)
+            self.assertEqual(wyde.hex, reference.hex)
+        
+        # Initialize with valid unsigned values
+        test_values = list()
+        test_values.append(0)
+        test_values.append(2**WYDE_SIZE-1)
+        for i in range(20):
+            test_values.append(randint(0+1, 2**WYDE_SIZE-1-1))
+        for i in test_values:
+            reference = BitArray(uint=i, length=WYDE_SIZE)
+            wyde = Wyde(uint=i)
+            self.assertEqual(wyde.length, WYDE_SIZE)
+            self.assertEqual(wyde.uint, i)
+            self.assertEqual(wyde.bin, reference.bin)
+            self.assertEqual(wyde.hex, reference.hex)
+        
+        # Initialize with invalid values
+        self.assertRaises(CreationError, Wyde, int=-2**(WYDE_SIZE-1)-1)
+        self.assertRaises(CreationError, Wyde, int=2**(WYDE_SIZE-1))
+        self.assertRaises(CreationError, Wyde, uint=-1)
+        self.assertRaises(CreationError, Wyde, uint=2**WYDE_SIZE)
+    
+    def testLogic(self):
+        '''
+        Verify that Wyde supports logical arithmetic.
+        '''
+        x = Wyde(uint=0b01010101)
+        y = Wyde(uint=0b10101010)
+        self.assertEqual((x&y).bin, '0000000000000000')
+        self.assertEqual((x|y).bin, '0000000011111111')
+        self.assertEqual((x^y).bin, '0000000011111111')
+    
+    def testAlgebra(self):
+        '''
+        Verify that Wyde supports algebra arithmetic.
+        '''
+        x = Wyde(uint=2)
+        y = Wyde(uint=3)
+        self.assertEqual((x+y).uint, 5)
+        self.assertEqual((y+x).int, 5)
+        self.assertEqual((x-y).int, -1)
+        self.assertEqual((y-x).int, 1)
+
+if __name__ == '__main__':
+    unittest.main()
