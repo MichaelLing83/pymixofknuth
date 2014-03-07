254ec7fccd69e149035eef8686b7e8bbe2ed3934
duo.ling.cn@gmail.com <duo.ling.cn@gmail.com>
2014-03-07 16:02:03 +0100

    Add class Register.

---
+++ src/Register.py
@@ -0,0 +1,3 @@
+﻿from Octa import Octa
+
+Register = Octa
---
+++ src/testRegister.py
@@ -0,0 +1,24 @@
+﻿import unittest
+
+class TestRegister(unittest.TestCase):
+    '''
+    As Octa test covers all, here are the extra tests that Register needs.
+    '''
+    
+    @classmethod
+    def setUpClass(cls):
+        print("\nStart testing %s" % __name__)
+    
+    @classmethod
+    def tearDownClass(cls):
+        print("\nFinish testing %s" % __name__)
+    
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+        
+
+if __name__ == '__main__':
+    unittest.main()
