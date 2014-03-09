1b9588300b6aff31e4eb525964cab1460b21a405
duo.ling.cn@gmail.com <duo.ling.cn@gmail.com>
2014-03-09 17:12:53 +0100

    Add memory in MMIX

--- src/MMIX.py
+++ src/MMIX.py
@@ -1,9 +1,11 @@
 ﻿from Register import Register
+from Memory import Memory
 
 class MMIX:
     ADDRESS_WIDTH_IN_BIT = 64
     REGISTER_BIT_WIDTH = 64
     def __init__(self):
+        # add registers
         self.general_purpose_registers = list()
         for i in range(256):
             self.general_purpose_registers.append(Register())
@@ -11,3 +13,6 @@ class MMIX:
         for i in range(32):
             self.special_purpose_registers.append(Register())
         self.special_purpose_register_names = ['rB', 'rD', 'rE', 'rH', 'rJ', 'rM', 'rR', 'rBB', 'rC', 'rN', 'rO', 'rS', 'rI', 'rT', 'rTT', 'rK', 'rQ', 'rU', 'rV', 'rG', 'rL', 'rA', 'rF', 'rP', 'rW', 'rX', 'rY', 'rZ', 'rWW', 'rXX', 'rYY', 'rZZ']
+        
+        # add memory
+        self.memory = Memory()
